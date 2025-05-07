a2c5e59503927a93e409abb74d057f8000982033
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2025-05-07 14:11:47 +0300

    scroll to top when navigating through pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import "@fontsource/inter/index.css";
 import "./index.css";
 import App from "./App.tsx";
+import ScrollToTop from "./components/ScrollToTop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
