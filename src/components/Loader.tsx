19ecee33779e32eefeef5201728ba841487137b7
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2025-05-07 18:39:49 +0300

    create Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+    return (
+      <div className="flex justify-center items-center h-64" aria-label="Loading">
+        <div className="w-12 h-12 border-4 border-fuchsia-600 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  };
+  
+  export default Loader;
+  
