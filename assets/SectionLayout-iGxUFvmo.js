17a0f213848c10cbc21a9e2734588457da4d7ffe
JoeClos <JoeClos@users.noreply.github.com>
2025-05-08 11:29:14 +0000

    deploy: 326e8713235ff8949ef7197b0289ef4fad5c7cfb

--- assets/SectionLayout-iGxUFvmo.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DqYWy3dQ.js";const m=({title:s,children:t})=>e.jsxs("section",{className:"pt-24 pb-16 px-4 sm:px-6 lg:px-8 max-w-screen-md mx-auto",children:[s&&e.jsx("h2",{className:"text-2xl font-semibold mb-6",children:s}),t]});export{m as S};
